ecf4c456b8d5440dd7e298d8e1a313811571b594
new-biee <anhvtth1907004@fpt.edu.vn>
2020-11-22 22:52:51 +0700

    Add all file:

---
+++ src/com/main/GenericStackClient.java
@@ -0,0 +1,12 @@
+package com.main;
+
+public class GenericStackClient {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of Integers");
+        MyGenericsStack.stackOfIntergers();
+        System.out.println("\n2. Stack of Strings");
+        MyGenericsStack.stackOfStrings();
+    }
+
+
+}
